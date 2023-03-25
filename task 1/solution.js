57b0fb9ae934add6852f6e69df53d33907302995
Ankur Tripathi <124662014+webdevankur@users.noreply.github.com>
2023-03-25 18:54:16 +0530

    Add files via upload

---
+++ task 1/solution.js
@@ -0,0 +1,8 @@
+//https://dev.to/
+
+var a = document.querySelector(".crayons-subtitle-2")
+a.innerText = "iNeuron"
+'iNeuron'
+var b = document.querySelector(".color-base-70")
+b.innerText = "i write code"
+'i write code'
