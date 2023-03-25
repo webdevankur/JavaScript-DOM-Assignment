99f2c96419d0520f1515111f5bd1e7497af47d02
Ankur Tripathi <124662014+webdevankur@users.noreply.github.com>
2023-03-25 19:14:51 +0530

    Add files via upload

---
+++ task 4/solution.js
@@ -0,0 +1,6 @@
+//https://www.oneplus.in/oneplus-11?&utm_source=google_search&utm_medium=cpc&utm_campaign=Smartoneplus11&utm_term=oneplus%20india&gclid=Cj0KCQjwt_qgBhDFARIsABcDjOfDtrr-bhWWtefmgs6kS1PI6YxfdVmIQeryC_nkkecNizeE_ANZZxEaApqeEALw_wcB
+
+var a = document.querySelector(".chat-box")
+ 
+a.innerText = "+917007585085"
+'+917007585085'
